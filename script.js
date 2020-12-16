19698dd637dfbea23b3d323f998d175edaa32877
n0h0m0 <aleksandregasparyan@gmail.com>
2020-12-16 18:01:15 +0400

    fixed import

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+//const { default: Axios } = require("axios");
+//var $ = require( "jquery" );
 
+import $ from 'jquery';
 
 $(document).ready(function (){
     $('body').css('background-color', 'blue');
     $('body').css('opacity', '50%');
+    $('p').css('font-size' , '24px');
+    
 })
